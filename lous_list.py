e522b56435e24fed9f6fffe9be0d393a24b1a31f
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2021-03-09 21:22:32 -0500

    Add files via upload

---
+++ lous_list.py
@@ -0,0 +1,33 @@
+import urllib.request
+
+
+def instructors(department):
+    list1 = []
+    if department == 'CS':
+        key = urllib.request.urlopen('http://cs1110.cs.virginia.edu/files/louslist/CS')
+        bytes1 = key.read()
+        text = bytes1.decode('utf-8')
+        rows = text.strip().split('\n')
+        data = rows[:]
+        for row in data:
+            cells = row.split(',')
+            for i in cells:
+                columns = i.split('|')
+                if columns[4] not in list1 and columns[4][-1] != '1':
+                    list1.append(columns[4])
+        list2 = sorted(list1)
+        return list2
+    elif department == 'ECE':
+        key = urllib.request.urlopen('http://cs1110.cs.virginia.edu/files/louslist/ECE')
+        bytes1 = key.read()
+        text = bytes1.decode('utf-8')
+        rows = text.strip().split('\n')
+        data = rows[:]
+        for row in data:
+            cells = row.split(',')
+            for i in cells:
+                columns = i.split('|')
+                if columns[4] not in list1 and columns[4][-1] != '1':
+                    list1.append(columns[4])
+        list2 = sorted(list1)
+        return list2
